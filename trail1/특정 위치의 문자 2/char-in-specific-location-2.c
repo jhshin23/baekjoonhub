442c7323ab42aa3dc449c4f2242ac16e07d98cfc
jhshin23 <180617768+jhshin23@users.noreply.github.com>
2026-09-25 20:20:37 +0900

    [Passed] 특정 위치의 문자 2 (2ms, 0MB)

--- trail1/특정 위치의 문자 2/char-in-specific-location-2.c
+++ trail1/특정 위치의 문자 2/char-in-specific-location-2.c
@@ -3,7 +3,7 @@
 int main() {
     // Please write your code here.
     enum idxOrder { second = 1, fifth = 4, eighth = 7, max = 10};
-    int arr[max] = {0};
+    char arr[max] = {0};
 
     for (int i = 0; i < max; i++) {
         scanf("%c ", &arr[i]);
